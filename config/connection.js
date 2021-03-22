d9b106df0b18444632edecc9a4b40326a9a77e3c
B Brucato <bebrucato@gmail.com>
2021-03-21 19:44:25 -0700

    added initial prompt

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,8 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+const inquirer = require('inquirer');
+const mySql = require('mysql');
+const consoleTable = require('console.table')
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -11,5 +14,15 @@ const sequelize = new Sequelize(
     port: 3306,
   }
 );
+
+function searching() {
+  inquirer
+  .prompt({
+    name:"initChoice",
+    type:"list",
+    message:"Please select how you would like to proceed.",
+    choices: ["View Departments", "View Employees", "View Employees By Department", "View Employees By Manager", "Add Employee", "Remove Employee", "Update Employee"]
+  })
+}
 
 module.exports = sequelize;
