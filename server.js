4ec4177fd094ca2353e9b00b93b0dedacf39672a
B Brucato <bebrucato@gmail.com>
2021-03-22 19:43:36 -0700

    employee tables are populating

--- server.js
+++ server.js
@@ -22,33 +22,33 @@ function searching() {
     name:"initChoice",
     type:"list",
     message:"Please select how you would like to proceed.",
-    choices: ["View Departments", "View Employees", "View Employees By Department", "View Employees By Manager", "Add Employee", "Remove Employee", "Update Employee", "End Session"]
+    choices: ["View Employees", "View Employees By Department", "View All Employees by Role", "Create a Department", "Create a Role", "Add Employee", "Update Employee", "End Session"]
   })
   .then(function(answer) {
     switch (answer.initChoice) {
-      case "View Departments":;
-        viewDepartments();
-        break;
-
         case "View Employees":
           viewEmployees();
           break;
-        
-        case "View Employees By Department":
-          employeesByDept();
-          break;
 
-        case "View Employees By Manager":
-          byManager();
-          break;
+        case "View Employees by Department":
+            viewAllDepartment();
+            break;
+
+        case "View All Employees by Role":
+            viewAllRoles();
+             break;
+
+        case "Create a Department":
+            createDepartment();
+            break;
+
+        case "Create a Role":
+           createRole();
+           break;
 
         case "Add Employee":
           addEmployee();
           break;
-
-        case "Remove Employee":
-        removeEmployee();
-        break;
 
         case "Update Employee":
           updateEmployee();
@@ -61,7 +61,24 @@ function searching() {
   })
 };
 
-function viewDepartments(){
+
+function viewEmployees() {
+  connection.query(
+    `SELECT employee.first_name, employee.last_name, roles.salary, roles.title, department.dept_name as "Department Name"
+    FROM employee_tracker_db.employee
+    INNER JOIN roles ON employee.roles_id = roles.id
+    INNER JOIN department ON roles.department_id = department.id`,
+
+    function (err, res) {
+      if (err) throw err;
+
+      console.table(res);
+      searching();
+    }
+  );
+};
+
+function viewAllDepartment(){
   connection.query(
     "SELECT department.dept_name FROM employee_tracker_db.department ",
     function (err, res) {
@@ -96,7 +113,7 @@ function viewDepartments(){
             if (err) throw err;
 
             console.table(res);
-            questions();
+            searching();
           }
         );
       });
@@ -104,21 +121,9 @@ function viewDepartments(){
 );
 };
 
-function viewEmployees() {
-  connection.query(
-    `SELECT employee.first_name, employee.last_name, roles.salary, roles.title, department.dept_name as "Department Name"
-    FROM employee_tracker_db.employee
-    INNER JOIN role ON employee.role_id = role.id
-    INNER JOIN department ON role.department_id = department.id`,
 
-    function (err, res) {
-      if (err) throw err;
 
-      console.table(res);
-      questions();
-    }
-  );
-}
+
 
 
 
