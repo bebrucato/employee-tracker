cb354f3fe81f9dd868111656abdc47e9346baa94
B Brucato <bebrucato@gmail.com>
2021-03-22 19:30:06 -0700

    employee seed

--- db/seed.sql
+++ db/seed.sql
@@ -9,3 +9,9 @@ VALUES ("Dark Arts Professor",100000,2),
         ("Potions Professor", 100000,3),
         ("History of Magic Professor",160000,1),
         ("Astronomy Professor",100000,4);
+
+INSERT INTO employee (first_name, last_name, roles_id) 
+VALUES ("Severus", "Snape", 1),
+        ("Horace", "Slughorn", 2),
+        ("Rubeus", "Hagrid", 4),
+        ("Minerva", "McGonagall", 3);
